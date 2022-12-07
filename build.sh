3f845a0029d474fb2ddcdad3a1716664e19a2b74
Liad Yosef <liad@duda.co>
2022-12-07 20:35:28 +0200

    feat: build for firefox

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-mkdir -p ../dist/v${VERSION}
 cd src
 
 VERSION=$(cat manifest.json | grep '"version":' | cut -d '"' -f 4)
 echo "Building version $VERSION"
+
+# create dist folder
+mkdir -p ../dist/v${VERSION}
 
 # build chrome
 zip -r ../dist/v${VERSION}/chrome-chatgpt-share-v${VERSION}.zip .
