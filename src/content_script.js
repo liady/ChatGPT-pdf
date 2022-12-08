81ebd01b3a790def106fb4ff7a363b379db8222b
adrianmarinwork <adrianmarinwork@gmail.com>
2022-12-08 19:48:16 +0100

    easier fix for issue #2 - fixes lists, texts and codeblocks

--- src/content_script.js
+++ src/content_script.js
@@ -70,22 +70,12 @@ function downloadThread({ as = Format.PNG } = {}) {
   const pixelRatio = window.devicePixelRatio;
   const minRatio = as === Format.PDF ? 2 : 2.5;
   window.devicePixelRatio = Math.max(pixelRatio, minRatio);
+
+  //Fix to the text shifting down when generating the canvas
+  document.body.style.lineHeight = '0.5';
+
   html2canvas(elements.thread, {
     letterRendering: true,
-    onclone: function (cloneDoc) {
-      //Make small fix of position to all the text containers
-      let listOfTexts = cloneDoc.getElementsByClassName("min-h-[20px]");
-      Array.from(listOfTexts).forEach((text) => {
-        text.style.position = "relative";
-        text.style.top = "-8px";
-      });
-
-      //Delete copy button from code blocks
-      let listOfCopyBtns = cloneDoc.querySelectorAll("button.flex");
-      Array.from(listOfCopyBtns).forEach(
-        (btn) => (btn.style.visibility = "hidden")
-      );
-    },
   }).then(async function (canvas) {
     elements.restoreLocation();
     window.devicePixelRatio = pixelRatio;
