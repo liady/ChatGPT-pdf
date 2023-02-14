431367b7044efa7c6d5fbcab1657249fa65d48a7
Ray McCann <ray@raymccann.com>
2023-02-14 16:10:09 -0500

    Revert "Refactor for maintainability"
    This reverts commit fde7b1d091364307f221ca9c10dc01855340dce4.

--- src/content_script.js
+++ src/content_script.js
@@ -1,101 +1,33 @@
-const Format = {
-  PNG: "png",
-  PDF: "pdf",
-};
-
-const TRY_AGAIN_BUTTON_HTML = `<button class="btn flex justify-center gap-2 btn-neutral" id="download-png-button">Try Again</button>`;
-const ACTIONS_AREA_SELECTOR = "form>div>div";
-const OPEN_SCREEN_SELECTOR = "h1.text-4xl";
-const CONVERSATION_SELECTOR = "form button>div";
-const FINISHED_CONVERSATION_SELECTOR = "form button>svg";
-const SHARE_BUTTONS_SELECTOR = "button[share-ext]";
-const DOWNLOAD_PNG_BUTTON_SELECTOR = "#download-png-button";
-const DOWNLOAD_PDF_BUTTON_SELECTOR = "#download-pdf-button";
-const DOWNLOAD_HTML_BUTTON_SELECTOR = "#download-html-button";
-
-
-const DEFAULT_FORMAT = Format.PNG;
-const MIN_PDF_PIXEL_RATIO = 2;
-const MIN_PNG_PIXEL_RATIO = 2.5;
-
+const buttonOuterHTMLFallback = `<button class="btn flex justify-center gap-2 btn-neutral" id="download-png-button">Try Again</button>`;
 async function init() {
-  clearIntervalIfExists();
-  setIntervalForButtons();
-}
-
-function clearIntervalIfExists() {
   if (window.buttonsInterval) {
     clearInterval(window.buttonsInterval);
   }
-}
-
-function setIntervalForButtons() {
   window.buttonsInterval = setInterval(() => {
-    const actionsArea = getActionsArea();
-    if (!actionsArea) return;
+    const actionsArea = document.querySelector("form>div>div");
+    if (!actionsArea) {
+      return;
+    }
     if (shouldAddButtons(actionsArea)) {
-      addActionsButtons(actionsArea);
+      let TryAgainButton = actionsArea.querySelector("button");
+      if (!TryAgainButton) {
+        const parentNode = document.createElement("div");
+        parentNode.innerHTML = buttonOuterHTMLFallback;
+        TryAgainButton = parentNode.querySelector("button");
+      }
+      addActionsButtons(actionsArea, TryAgainButton);
     } else if (shouldRemoveButtons()) {
       removeButtons();
     }
   }, 200);
 }
 
-function getActionsArea() {
-  return document.querySelector(ACTIONS_AREA_SELECTOR);
-}
-
-function addActionsButtons(actionsArea) {
-  const TryAgainButton = getTryAgainButton(actionsArea);
-  addDownloadPngButton(actionsArea, TryAgainButton);
-  addDownloadPdfButton(actionsArea, TryAgainButton);
-  addShareLinkButton(actionsArea, TryAgainButton);
-}
-
-function getTryAgainButton(actionsArea) {
-  let TryAgainButton = actionsArea.querySelector("button");
-  if (!TryAgainButton) {
-    const parentNode = document.createElement("div");
-    parentNode.innerHTML = TRY_AGAIN_BUTTON_HTML;
-    TryAgainButton = parentNode.querySelector("button");
-  }
-  return TryAgainButton;
-}
-
-function addDownloadPngButton(actionsArea, TryAgainButton) {
-  const downloadButton = TryAgainButton.cloneNode(true);
-  downloadButton.id = DOWNLOAD_PNG_BUTTON_SELECTOR;
-  downloadButton.setAttribute("share-ext", "true");
-  downloadButton.innerText = "Generate PNG";
-  downloadButton.onclick = downloadThread;
-  actionsArea.appendChild(downloadButton);
-}
-
-function addDownloadPdfButton(actionsArea, TryAgainButton) {
-  const downloadPdfButton = TryAgainButton.cloneNode(true);
-  downloadPdfButton.id = DOWNLOAD_PDF_BUTTON_SELECTOR;
-  downloadPdfButton.setAttribute("share-ext", "true");
-  downloadPdfButton.innerText = "Download PDF";
-  downloadPdfButton.onclick = () => downloadThread({ as: Format.PDF });
-  actionsArea.appendChild(downloadPdfButton);
-}
-
-function addShareLinkButton(actionsArea, TryAgainButton) {
-  const exportHtml = TryAgainButton.cloneNode(true);
-  exportHtml.id = DOWNLOAD_HTML_BUTTON_SELECTOR;
-  exportHtml.setAttribute("share-ext", "true");
-  exportHtml.innerText = "Share Link";
-  exportHtml.onclick = sendRequest;
-  actionsArea.appendChild(exportHtml);
-}
-
-
 function shouldRemoveButtons() {
-  const isOpenScreen = document.querySelector(OPEN_SCREEN_SELECTOR);
+  const isOpenScreen = document.querySelector("h1.text-4xl");
   if (isOpenScreen) {
     return true;
   }
-  const inConversation = document.querySelector(CONVERSATION_SELECTOR);
+  const inConversation = document.querySelector("form button>div");
   if (inConversation) {
     return true;
   }
@@ -103,10 +35,12 @@ function shouldRemoveButtons() {
 }
 
 function shouldAddButtons(actionsArea) {
-  if (shouldRemoveButtons()) {
+  // Blinking button fix by @pedrokohler
+  if(shouldRemoveButtons()){
     return false;
   }
 
+  // first, check if there's a "Try Again" button and no other buttons
   const buttons = actionsArea.querySelectorAll("button");
   const hasTryAgainButton = Array.from(buttons).some((button) => {
     return !button.id?.includes("download");
@@ -115,13 +49,15 @@ function shouldAddButtons(actionsArea) {
     return true;
   }
 
-  const isOpenScreen = document.querySelector(OPEN_SCREEN_SELECTOR);
+  // otherwise, check if open screen is not visible
+  const isOpenScreen = document.querySelector("h1.text-4xl");
   if (isOpenScreen) {
     return false;
   }
 
-  const finishedConversation = document.querySelector(FINISHED_CONVERSATION_SELECTOR);
-  const hasShareButtons = actionsArea.querySelectorAll(SHARE_BUTTONS_SELECTOR);
+  // check if the conversation is finished and there are no share buttons
+  const finishedConversation = document.querySelector("form button>svg");
+  const hasShareButtons = actionsArea.querySelectorAll("button[share-ext]");
   if (finishedConversation && !hasShareButtons.length) {
     return true;
   }
@@ -129,38 +65,59 @@ function shouldAddButtons(actionsArea) {
   return false;
 }
 
+const Format = {
+  PNG: "png",
+  PDF: "pdf",
+};
+
 function removeButtons() {
-  const downloadButton = document.querySelector(DOWNLOAD_PNG_BUTTON_SELECTOR);
+  const downloadButton = document.getElementById("download-png-button");
+  const downloadPdfButton = document.getElementById("download-pdf-button");
+  const downloadHtmlButton = document.getElementById("download-html-button");
   if (downloadButton) {
     downloadButton.remove();
   }
-
-  const downloadPdfButton = document.querySelector(DOWNLOAD_PDF_BUTTON_SELECTOR);
   if (downloadPdfButton) {
     downloadPdfButton.remove();
   }
-
-  const downloadHtmlButton = document.querySelector(DOWNLOAD_HTML_BUTTON_SELECTOR);
   if (downloadHtmlButton) {
     downloadHtmlButton.remove();
   }
 }
 
-
-function downloadThread({ as = DEFAULT_FORMAT } = {}) {
-  if (as === Format.PDF) {
-    downloadThreadPdf();
-  } else {
-    downloadThreadPng();
-  }
+function addActionsButtons(actionsArea, TryAgainButton) {
+  const downloadButton = TryAgainButton.cloneNode(true);
+  downloadButton.id = "download-png-button";
+  downloadButton.setAttribute("share-ext", "true");
+  downloadButton.innerText = "Generate PNG";
+  downloadButton.onclick = () => {
+    downloadThread();
+  };
+  actionsArea.appendChild(downloadButton);
+  const downloadPdfButton = TryAgainButton.cloneNode(true);
+  downloadPdfButton.id = "download-pdf-button";
+  downloadButton.setAttribute("share-ext", "true");
+  downloadPdfButton.innerText = "Download PDF";
+  downloadPdfButton.onclick = () => {
+    downloadThread({ as: Format.PDF });
+  };
+  actionsArea.appendChild(downloadPdfButton);
+  const exportHtml = TryAgainButton.cloneNode(true);
+  exportHtml.id = "download-html-button";
+  downloadButton.setAttribute("share-ext", "true");
+  exportHtml.innerText = "Share Link";
+  exportHtml.onclick = () => {
+    sendRequest();
+  };
+  actionsArea.appendChild(exportHtml);
 }
 
-function downloadThreadPng() {
+function downloadThread({ as = Format.PNG } = {}) {
   const elements = new Elements();
   elements.fixLocation();
-
   const pixelRatio = window.devicePixelRatio;
-  window.devicePixelRatio = Math.max(pixelRatio, MIN_PNG_PIXEL_RATIO);
+  const minRatio = as === Format.PDF ? 2 : 2.5;
+  window.devicePixelRatio = Math.max(pixelRatio, minRatio);
 
   html2canvas(elements.thread, {
     letterRendering: true,
@@ -169,12 +126,16 @@ function downloadThreadPng() {
     window.devicePixelRatio = pixelRatio;
     const imgData = canvas.toDataURL("image/png");
     requestAnimationFrame(() => {
-      handlePng(imgData);
+      if (as === Format.PDF) {
+        return handlePdf(imgData, canvas, pixelRatio);
+      } else {
+        handleImg(imgData);
+      }
     });
   });
 }
 
-function handlePng(imgData) {
+function handleImg(imgData) {
   const binaryData = atob(imgData.split("base64,")[1]);
   const data = [];
   for (let i = 0; i < binaryData.length; i++) {
@@ -184,28 +145,12 @@ function handlePng(imgData) {
   const url = URL.createObjectURL(blob);
 
   window.open(url, "_blank");
+
+  //   const a = document.createElement("a");
+  //   a.href = url;
+  //   a.download = "chat-gpt-image.png";
+  //   a.click();
 }
-
-
-function downloadThreadPdf() {
-  const elements = new Elements();
-  elements.fixLocation();
-
-  const pixelRatio = window.devicePixelRatio;
-  window.devicePixelRatio = Math.max(pixelRatio, MIN_PDF_PIXEL_RATIO);
-
-  html2canvas(elements.thread, {
-    letterRendering: true,
-  }).then(async function (canvas) {
-    elements.restoreLocation();
-    window.devicePixelRatio = pixelRatio;
-    const imgData = canvas.toDataURL("image/png");
-    requestAnimationFrame(() => {
-      handlePdf(imgData, canvas, pixelRatio);
-    });
-  });
-}
-
 
 function handlePdf(imgData, canvas, pixelRatio) {
   const { jsPDF } = window.jspdf;
@@ -229,7 +174,7 @@ class Elements {
     this.spacer = document.querySelector(".w-full.h-32.md\\:h-48.flex-shrink-0");
     this.thread = document.querySelector("main>div.flex-1>div>div");
     this.feedback = this.thread.querySelectorAll(
-        "div>div.text-base>div:nth-of-type(2)>div:nth-of-type(2)"
+      "div>div.text-base>div:nth-of-type(2)>div:nth-of-type(2)"
     );
     this.positionForm = document.querySelector("form").parentNode;
     // this.styledThread = document.querySelector("main");
@@ -285,7 +230,7 @@ function selectElementByClassPrefix(classPrefix) {
 async function sendRequest() {
   const data = getData();
   const uploadUrlResponse = await fetch(
-      "https://chatgpt-static.s3.amazonaws.com/url.txt"
+    "https://chatgpt-static.s3.amazonaws.com/url.txt"
   );
   const uploadUrl = await uploadUrlResponse.text();
   fetch(uploadUrl, {
@@ -295,20 +240,20 @@ async function sendRequest() {
     },
     body: JSON.stringify(data),
   })
-      .then((response) => response.json())
-      .then((data) => {
-        window.open(data.url, "_blank");
-      });
+    .then((response) => response.json())
+    .then((data) => {
+      window.open(data.url, "_blank");
+    });
 }
 
 function getData() {
   const globalCss = getCssFromSheet(
-      document.querySelector("link[rel=stylesheet]").sheet
+    document.querySelector("link[rel=stylesheet]").sheet
   );
   const localCss =
-      getCssFromSheet(
-          document.querySelector(`style[data-emotion]`).sheet
-      ) || "body{}";
+    getCssFromSheet(
+      document.querySelector(`style[data-emotion]`).sheet
+    ) || "body{}";
   const data = {
     main: document.querySelector("main").outerHTML,
     // css: `${globalCss} /* GLOBAL-LOCAL */ ${localCss}`,
@@ -320,14 +265,14 @@ function getData() {
 
 function getCssFromSheet(sheet) {
   return Array.from(sheet.cssRules)
-      .map((rule) => rule.cssText)
-      .join("");
+    .map((rule) => rule.cssText)
+    .join("");
 }
 
 // run init
 if (
-    document.readyState === "complete" ||
-    document.readyState === "interactive"
+  document.readyState === "complete" ||
+  document.readyState === "interactive"
 ) {
   init();
 } else {
